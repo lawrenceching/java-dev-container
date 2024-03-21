9033b6add5d6d621e530c1a895ed97efa4689927
Lawrence Ching <lawrence.ching.zh@gmail.com>
2024-03-22 00:42:11 +0800

    seconds commit

--- bin/init.sh
+++ bin/init.sh
@@ -3,11 +3,11 @@
 cp /tmp/.bashrc ~/.bashrc
 
 apt update
-apt install -y ca-certificates
 
-bash /tmp/use-source.sh /tmp/source.list.mainland-china
+#apt install -y ca-certificates
+#bash /tmp/use-source.sh /tmp/source.list.mainland-china
+#apt update
 
-apt update
 apt install -y openssh-server neovim htop tmux wget curl jq
 
 bash /tmp/install-java.sh
